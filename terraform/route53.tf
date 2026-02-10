cb7d1c624fc75e791085b0080883f09afbc9642d
Sean <162363+seanezell@users.noreply.github.com>
2026-02-09 22:56:50 -0800

    bug: deal with the CNAME cert validation alert

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,3 +1,20 @@
+# ACM certificate validation
+resource "aws_route53_record" "cert_validation" {
+    for_each = {
+        for dvo in data.aws_acm_certificate.issued.domain_validation_options : dvo.domain_name => {
+            name   = dvo.resource_record_name
+            record = dvo.resource_record_value
+            type   = dvo.resource_record_type
+        }
+    }
+
+    zone_id = var.route53_zone_id
+    name    = each.value.name
+    type    = each.value.type
+    records = [each.value.record]
+    ttl     = 60
+}
+
 # website records
 resource "aws_route53_record" "www" {
     zone_id = var.route53_zone_id  # hosted zone ID for seanezell.com
